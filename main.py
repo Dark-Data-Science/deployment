00b8e8648ed91782eba0f6d55c23a4e7a00f6126
CodeWizard-bip <atlasnexus193@gmail.com>
2023-11-28 03:01:25 -0300

    ultima

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import uvicorn
-from fastapi import FastAPI
-import pandas as pd
-from fastapi.responses import JSONResponse
 from fastapi import FastAPI, HTTPException
+import pandas as pd
+import fastparquet
+import dask.dataframe as dd
+import dask
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 
@@ -119,8 +121,8 @@ df_recomendado = pd.read_parquet('Data/Recommend_Users2.parquet')
 
 
 
-@app.get('/UsersNotRecommend/{anio}')
-async def UsersNotRecommend(anio: int):
+@app.get('/UsersWorstDeveloper/{anio}')
+async def UsersWorstDeveloper(anio: int):
     try:
         # Filtrar por el año dado
         df_year = df_recomendado[df_recomendado['Año_estreno'] == anio]
@@ -148,4 +150,96 @@ async def UsersNotRecommend(anio: int):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Se produjo un error inesperado: {str(e)}")
+    
 
+
+analisis_sentimiento = pd.read_parquet('Data/Sentiment.parquet')
+
+@app.get('/sentiment_analysis/{desarrolladora}')
+def sentiment_analysis(desarrolladora: str):
+    # Mapeo de los valores de 'sentiment_analisis' a las cadenas de texto correspondientes
+    sentiment_mapping = {0: 'Negative', 1: 'Neutral', 2: 'Positive'}
+
+    # Filtrar el dataframe por la desarrolladora especificada
+    df_developer = analisis_sentimiento[analisis_sentimiento['developer'] == desarrolladora]
+
+    # Si no hay datos para la desarrolladora, devolver un mensaje indicando esto
+    if df_developer.empty:
+        return {desarrolladora: "No hay datos para la desarrolladora."}
+
+    # Reemplazar los valores en 'sentiment_analisis' con las cadenas de texto correspondientes
+    df_developer['sentiment_analisis'] = df_developer['sentiment_analisis'].map(sentiment_mapping)
+
+    # Contar el número de registros de reseñas de usuarios que se encuentran categorizados con un análisis de sentimiento
+    sentiment_counts = df_developer['sentiment_analisis'].value_counts().to_dict()
+
+    # Crear la lista de salida en el formato deseado
+    output_list = [f"{key} = {value}" for key, value in sentiment_counts.items()]
+
+    return {desarrolladora: output_list}
+
+
+modelado_item = pd.read_parquet('Data/Sentiment.parquet')
+
+
+@app.get("/recomendacion/{id_producto}")
+async def get_recomendacion(id_producto: int):
+    # Filtrar el DataFrame modelado_item para obtener las recomendaciones de juegos
+    # asociadas con el juego específico identificado por su id.
+    recomendaciones = modelado_item[modelado_item['id'] == id_producto]['recomendaciones'].iloc[0]
+
+    # Verificar si hay recomendaciones disponibles para el juego dado
+    if len(recomendaciones) > 0:
+        # Crear un diccionario numerado de recomendaciones para una presentación más organizada
+        recomendaciones_dict = {i + 1: juego for i, juego in enumerate(recomendaciones)}
+        # Devolver el diccionario de recomendaciones numeradas
+        return recomendaciones_dict
+    else:
+        # Si no hay recomendaciones disponibles, devolver un diccionario vacío
+        return {}
+
+
+
+
+
+
+@app.get("/recomendacion_juego/{product_id}")
+async def recomendacion_juego(product_id: int):
+    try:
+        analisis_sentimiento = pd.read_csv('Data/Sentiment.csv')
+
+        target_game = analisis_sentimiento[analisis_sentimiento['item_id'] == product_id]
+
+        if target_game.empty:
+            return {"message": "No se encontró el juego de referencia."}
+
+        target_game_tags_and_genres = ' '.join(target_game['tags'].fillna('').astype(str) + ' ' + target_game['genres'].fillna('').astype(str))
+
+        tfidf_vectorizer = TfidfVectorizer()
+
+        chunk_size = 100   
+        similarity_scores = None
+
+        for chunk in pd.read_csv('Data/Sentiment.csv', chunksize=chunk_size):
+            chunk_tags_and_genres = chunk['tags'].fillna('').astype(str) + ' ' + chunk['genres'].fillna('').astype(str)
+            games_to_compare = [target_game_tags_and_genres] + chunk_tags_and_genres.tolist()
+
+            tfidf_matrix = tfidf_vectorizer.fit_transform(games_to_compare)
+
+            if similarity_scores is None:
+                similarity_scores = cosine_similarity(tfidf_matrix)
+            else:
+                similarity_scores = cosine_similarity(tfidf_matrix)
+
+        if similarity_scores is not None:
+            similar_games_indices = similarity_scores[0].argsort()[::-1]
+
+            num_recommendations = 5
+            recommended_games = analisis_sentimiento.loc[similar_games_indices[1:num_recommendations + 1]]
+
+            return recommended_games[['app_name']].to_dict(orient='records')
+
+        return {"message": "No se encontraron juegos similares."}
+
+    except Exception as e:
+        return {"message": f"Error: {str(e)}"}
